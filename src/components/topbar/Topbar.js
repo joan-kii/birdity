5934bbce768349d2bd2ad2351381d47f1e4e8fc0
joan-kii <joan.sb81@gmail.com>
2021-04-23 05:25:38 +0200

    añade router

--- src/components/topbar/Topbar.js
+++ src/components/topbar/Topbar.js
@@ -2,15 +2,22 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import { Link as RouterLink, BrowserRouter as Router } from 'react-router-dom';
+import Link from '@material-ui/core/Link';
 import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles((theme) => ({
   grow: {
       flexGrow: 1,
   },
+  topbar: {
+    backgroundColor: theme.palette.white,
+  },
   logo: {
     fontSize: 30,
-  }
+    color: theme.palette.secondary.dark,
+    underline: 'none',
+  }, 
 }));
 
 const Topbar = () => {
@@ -20,10 +27,14 @@ const Topbar = () => {
   return (
     <div className={classes.grow}>
       <AppBar position='static'>
-        <Toolbar>
-          <Typography className={classes.logo}>
-            Bird<span color='secondary'>ity</span>
-          </Typography>
+        <Toolbar className={classes.topbar}>
+          <Router>
+            <Link component={RouterLink} className={classes.logo} to="/">
+              <Typography>
+                Birdity
+              </Typography>
+            </Link>
+          </Router>
         </ Toolbar>
       </ AppBar>
     </div>
