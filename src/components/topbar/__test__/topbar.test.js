4020f0072f969ea05f7574b2b07f60caffef6a68
joan-kii <joan.sb81@gmail.com>
2021-04-24 18:05:31 +0200

    separa tests

--- src/components/topbar/__test__/topbar.test.js
+++ src/components/topbar/__test__/topbar.test.js
@@ -7,8 +7,16 @@ import Topbar from './../Topbar';
 describe('Topbar Component', () => {
   test('topbar renders fine', () => {
     render(<Topbar />);
+  });
+
+  test('Logo and slogan renders fine', () => {
+    render(<Topbar />);
     expect(screen.getByText('Birdity')).toBeInTheDocument();
     expect(screen.getByText('The Birdwatching Social Network')).toBeInTheDocument();
+  });
+  
+  test('Buttons renders fine', () => {
+    render(<Topbar />);
     expect(screen.getByTestId('chatButton')).toHaveTextContent('Chat');
     expect(screen.getByTestId('exploreButton')).toHaveTextContent('Explore');
   });
