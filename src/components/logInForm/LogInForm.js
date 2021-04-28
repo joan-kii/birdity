4598fe57d763ce00f046afe067f6686203a35a54
joan-kii <joan.sb81@gmail.com>
2021-04-28 05:23:23 +0200

    finaliza sign up modal. crea log in modal

--- src/components/logInForm/LogInForm.js
+++ src/components/logInForm/LogInForm.js
@@ -49,10 +49,15 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
-const SignUpForm = () => {
+const LogInForm = () => {
 
   const classes = useStyles();
 
+  /* const toggleModal = () => {
+    setOpenSignUpForm(true);
+    setOpenLogInForm(false);
+  };
+ */
   return (
     <Container>
       <Paper className={classes.paper}>
@@ -64,7 +69,7 @@ const SignUpForm = () => {
         component='h4'
         className={classes.title}
         color='primary'>
-          Sign Up
+          Log in
         </Typography>
         <form className={classes.form}>
           <Grid container spacing={4}>
@@ -73,10 +78,10 @@ const SignUpForm = () => {
                 size='large' 
                 aria-label='contained button group'>
                 <Button endIcon={<FacebookIcon />}>
-                  Sign Up with Facebook
+                  Log in with Facebook
                 </Button>
                 <Button startIcon={<TwitterIcon />}>
-                  Sign Up with Twitter
+                  Log in with Twitter
                 </Button>
               </ButtonGroup>
               <Button
@@ -86,7 +91,7 @@ const SignUpForm = () => {
                 startIcon={<SvgIcon htmlColor='inherit'>
                   <svg role="img" viewBox="0 0 28 28" xmlns="http://www.w3.org/2000/svg"><title>Google icon</title><path d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"/></svg>
                 </SvgIcon>}>
-                Sign Up with Google
+                Log in with Google
               </Button>
             </Grid>
             <Grid item xs={12}>
@@ -107,15 +112,6 @@ const SignUpForm = () => {
                 variant='outlined'
                 />
             </Grid>
-            <Grid item xs={12}>
-              <TextField required
-                id='confirmPassword'
-                type='password'
-                fullWidth
-                label='Confirm Password'
-                variant='outlined'
-                />
-            </Grid>
           </Grid>
           <Button 
             type='submit'
@@ -123,14 +119,15 @@ const SignUpForm = () => {
             variant='contained'
             size='large'
             fullWidth>
-              Sign Up
+              Log in
           </Button>
           <Grid container justify='flex-end'>
             <Grid item>
               <Link 
                 variant='subtitle1'
-                className={classes.text}>
-                  Already have an account? Log in.
+                className={classes.text}
+                /* onClick={toogleModal} */>
+                  Need an account? Sign up.
                 </Link>
             </Grid>
           </Grid>
@@ -140,4 +137,4 @@ const SignUpForm = () => {
   )
 };
 
-export default SignUpForm;
+export default LogInForm;
