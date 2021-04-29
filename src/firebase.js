a43c86ab6397edab1e53ab9f2c6ac94b37e6ebeb
joan-kii <joan.sb81@gmail.com>
2021-04-29 05:24:49 +0200

    bug firebase apiKey

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,4 @@
+import { Functions } from '@material-ui/icons';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firebase-firestore'; 
@@ -8,8 +9,10 @@ const app = firebase.initializeApp({
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 });
+
+console.log(app.options.apiKey)
 
 export const auth = app.auth();
 export const db = app.firestore();
