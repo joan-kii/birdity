90557f12014b6f414098f2d2b5ebc93c8e52123e
joan-kii <joan.sb81@gmail.com>
2021-06-04 18:19:55 +0200

    soluciona bug render userPosts

--- src/components/userPosts/UserPosts.js
+++ src/components/userPosts/UserPosts.js
@@ -1,4 +1,5 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 import { db } from '../../firebase';
 import { useAuth } from '../../context/Context';
@@ -8,30 +9,37 @@ const UserPosts = () => {
 
   const { currentUser } = useAuth();
 
-  const postRefs = [];
+  const [userPostRefs, setUserPostsRefs] = useState([]);
+  const [arePostsLoaded, setArePostsLoaded] = useState(false);
 
   useEffect(() => {
+
     async function getUserPosts() {
       await db.collection('users').doc(currentUser.uid).get().then((doc) => {
-        const posts  = doc.data().posts;
-        posts.forEach((post, index) => {
-          postRefs.push(
-            <PostCard 
-              index={index}
-              docRef={post} />
-          )
-        });
+        doc.data().posts.forEach(async (post) => {
+          await db.collection('posts').doc(post.id).get().then((docRef) => {
+            setUserPostsRefs(prevDocRef => {
+              return [...prevDocRef, docRef]
+            })
+          })
+        })
       }).catch((err) => {
         console.error(err);
       })
+      setArePostsLoaded(true)
     }
     getUserPosts();
-  }, [postRefs])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
-  console.log(postRefs)
   return (
     <div>
-      {postRefs}
+      {arePostsLoaded ? userPostRefs.map((docRef, index) => {
+        return <PostCard key={index} docRef={docRef} />}) :
+        <CircularProgress 
+          style={{margin: '35%',
+                  marginTop: '25%',}} />
+      }
     </div>
   )
 };
